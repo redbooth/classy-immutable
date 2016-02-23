159c00a25dcf735221d83ef1c91f066605c6c688
Barry Shapira <barry@aerofs.com>
2016-02-22 17:54:57 -0800

    Importing files.  Still need tests.

--- README.md
+++ README.md
@@ -1,2 +1,64 @@
 # classy-immutable
 Immutable instances of ES6 classes.
+
+## Examples
+### Setup
+
+```
+let Immutable = require('classy-immutable');
+
+class MyImmutableClass extends Immutable {
+    constructor(x, y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    get sum() {
+        return this.x + this.y;
+    }
+}
+```
+
+### Acts like a standard ES6 class.
+```
+let mic = MyImmutableClass.create(5, 10);
+mic.x === 5; // true
+mic.y === 10; // true
+mic.sum === 15; // true
+```
+
+### except that setters are no-ops
+```
+mic.x = 100;
+mic.x === 5; // true
+```
+
+### use the 'set', 'setIn', or 'merge' methods instead
+```
+let mic2 = mic.set('x', 100);
+mic2.x === 100; // true
+mic2.y === 10; // true
+mic2.sum === 110; // true
+```
+
+### new immutable instances are created with each set operation
+```
+mic === mic2;  // false
+```
+
+### inheritance works as expected
+```
+mic instanceof MyImmutableClass; // true
+mic2 instanceof MyImmutableClass; // true
+mic instanceof Immutable; // true
+mic2 instanceof Immutable; // true
+```
+
+## Methods
+static create
+static object
+set
+setIn
+merge
+without
+asMutable
---
+++ index.js
@@ -0,0 +1,78 @@
+'use strict';
+
+function deepFreeze(obj) {
+    for (let key in obj) {
+        let prop = obj[key];
+        if (typeof prop === 'object' && prop !== null && !Object.isFrozen(prop)) {
+            obj[key] = deepFreeze(prop);
+        }
+    }
+    return Object.freeze(obj);
+}
+
+function deepUnfreeze(obj) {
+    let clone = new obj.constructor();
+    for (let key in obj) {
+        let prop = obj[key];
+        if (typeof prop === 'object' && prop !== null) {
+            clone[key] = deepUnfreeze(prop);
+        } else {
+            clone[key] = prop;
+        }
+    }
+    return clone;
+}
+
+function setIn(obj = {}, keys = [], val) {
+    let clone = Object.assign(new obj.constructor(), obj);
+    switch (keys.length) {
+        case 0:
+            break;
+        case 1:
+            clone[keys[0]] = val;
+            break;
+        default:
+            clone[keys[0]] = setIn(obj[keys[0]], keys.slice(1), val);
+            break;
+    }
+    return clone;
+}
+
+class Immutable {
+    static create(...args) {
+        return deepFreeze(new this(...args));
+    }
+
+    static object(obj = {}) {
+        let clone = Object.assign(new Immutable(), obj);
+        return deepFreeze(clone);
+    }
+
+    set(key, val) {
+        let obj = {};
+        obj[key] = val;
+        return this.merge(obj);
+    }
+
+    setIn(keys = [], val) {
+        let clone = setIn(this, keys, val);
+        return deepFreeze(clone);
+    }
+
+    merge(that = {}) {
+        let clone = Object.assign(new this.constructor(), this, that);
+        return deepFreeze(clone);
+    }
+
+    without(keys = []) {
+        let clone = Object.assign(new this.constructor(), this);
+        keys.forEach(key => delete clone[key]);
+        return deepFreeze(clone);
+    }
+
+    asMutable() {
+        return deepUnfreeze(this);
+    }
+}
+
+module.exports = Immutable;
